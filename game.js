c8499247a0c95b2d87f121e41be24b3be28b9b56
rastringer <ra1stringer@gmail.com>
2015-10-02 10:56:30 -0700

    style/DOM revisions

--- game.js
+++ game.js
@@ -10,11 +10,10 @@ function ques1() {
   var question1 = prompt("Did I grow up in Hawaii?").toUpperCase();
   console.log('Question 1 repsonse: ' + question1);
   if (question1 === 'YES' || question1 === 'Y') {
-    one.innerHTML = "Nope, I grew up in the U.K.";
-  }
-  else {
-    one.innerHTML = "Correct!";
-    count++;
+  one.innerHTML = "Nope, I grew up in the U.K.";
+  } else {
+  one.innerHTML = "Correct!";
+  count++;
   };
 }
 
@@ -22,11 +21,10 @@ function ques2() {
   var question2 = prompt("Do I speak Mandarin?").toUpperCase();
   console.log('Question 2 response: ' + question2);
   if (question2 === 'YES' || question2 === 'Y') {
-    two.innerHTML = "I wish! Unfortunately I don't speak Mandarin."
-  }
-  else {
-	 two.innerHTML ="Correct";
-	 count++;
+  two.innerHTML = "I wish! Unfortunately I don't speak Mandarin."
+  } else {
+	two.innerHTML ="Correct";
+	count++;
   };
 }
 
@@ -34,18 +32,15 @@ function ques3() {
   var question3 = prompt("Do I own a motorcycle?").toUpperCase();
   console.log('Question 3 response: ' + question3);
   if (question3 === 'YES' || question3 ===  'Y') {
-	 three.innerHTML = "Incorrect. I do have a trusty mountain bike however."
-  }
-  else {
-	 three.innerHTML = "Correct";
-	 count++;
+	three.innerHTML = "Incorrect. I do have a trusty mountain bike however."
+  } else {
+	three.innerHTML = "Correct";
+	count++;
   };
 }
 
 ques1();
-
 ques2();
-
 ques3();
 
-alert("Congratulations, " + name + ", you answered " + count + " questions out of 3 correctly.");
+four.innerHTML = "Congratulations, " + name + ", you answered " + count + " questions out of 3 correctly.";
